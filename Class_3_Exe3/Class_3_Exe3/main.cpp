a8a34039d3bc8fe61a0a8b77986758880e884899
LeviKing98 <1105600952@qq.com>
2020-03-06 14:31:02 +0800

    Add

--- Class_3_Exe3/Class_3_Exe3/main.cpp
+++ Class_3_Exe3/Class_3_Exe3/main.cpp
@@ -1,2 +1,18 @@
 #include <opencv.hpp>
 using namespace cv;
+
+int main()
+{
+	cv::Mat dispMat;
+	cv::Point pt1, pt2;
+	line(dispMat, pt1, pt2, CV_RGB(255, 0, 0), 1, 8, 0);
+
+	cv::Rect rect;
+	rect.x = 10;
+	rect.y = 10;
+	rect.width=640;
+	rect.height=320;
+	rectangle(dispMat, rect, CV_RGB(255, 0, 0), 1, 8, 0);
+
+	waitKey(0);
+}
