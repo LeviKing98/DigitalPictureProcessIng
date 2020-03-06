e8709795ed73f46b0af566898771d12a07cbc05a
LeviKing98 <1105600952@qq.com>
2020-03-06 14:52:49 +0800

    Add

---
+++ Class_3_Exe4/Class_3_Exe4/main.cpp
@@ -0,0 +1,19 @@
+#include <opencv.hpp>
+using namespace cv;
+
+int main()
+{
+	float Pix[256];
+	cv::Mat srcMat = imread("G:\\jlw\\timg.jpg");
+	int height = srcMat.rows;
+	int width = srcMat.cols;
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
+			uchar average = (srcMat.at<Vec3b>(j, i)[0]
+				+ srcMat.at<Vec3b>(j, i)[1]
+				+ srcMat.at<Vec3b>(j, i)[2]) / 3;
+			Pix[average]++;
+		}
+	}
+	waitKey(0);
+}
