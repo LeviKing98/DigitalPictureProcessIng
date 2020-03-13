e2cc7bf07f305d67519b77b9dd7093650507ca59
LeviKing98 <1105600952@qq.com>
2020-03-13 13:55:53 +0800

    1

--- Class_4_Exp2/Class_4_Exp2/main.cpp
+++ Class_4_Exp2/Class_4_Exp2/main.cpp
@@ -1,2 +1,16 @@
 #include <opencv.hpp>
+#include <iostream>
 using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat result_1, result_2;
+	Mat srcMat = imread("G:\\jlw\\timg.jpg", 0);
+	threshold(srcMat, result_1,100,255,THRESH_BINARY);
+	adaptiveThreshold(srcMat, result_2, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 15, 10);
+	imshow("src", srcMat);
+	imshow("res_1", result_1);
+	imshow("res_2", result_2);
+	waitKey(0);
+}
