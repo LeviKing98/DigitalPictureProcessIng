05bff870d923281963dbaa703cb7f797bcb97920
LeviKing98 <1105600952@qq.com>
2020-03-20 13:56:46 +0800

    1

--- Class_5_Exe1/Class_5_Exe1/main.cpp
+++ Class_5_Exe1/Class_5_Exe1/main.cpp
@@ -7,12 +7,14 @@ int main()
 {
 	Mat result_1, result_2, result_3, result_4;
 	Mat srcMat = imread("G:\\jlw\\coin.png", 0);
+	Mat dstMat ;
 	Mat Core = getStructuringElement(MORPH_RECT,Size(3,3));
-	erode(srcMat, result_1, Core);
-	dilate(srcMat, result_2, Core);
-	morphologyEx(srcMat, result_3, MORPH_OPEN,Core);
-	morphologyEx(srcMat, result_4, MORPH_CLOSE, Core);
-	//threshold(srcMat, result_1, 100, 255, THRESH_BINARY);
+	threshold(srcMat, dstMat, 100, 255, THRESH_BINARY);
+	erode(dstMat, result_1, Core);
+	dilate(dstMat, result_2, Core);
+	morphologyEx(dstMat, result_3, MORPH_OPEN,Core);
+	morphologyEx(dstMat, result_4, MORPH_CLOSE, Core);
+	
 	//adaptiveThreshold(srcMat, result_2, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 15, 10);
 	imshow("src", srcMat);
     imshow("res_1", result_1);
