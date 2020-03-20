003ae41dcc4c7086783d852e304b76e4cfa38b2b
LeviKing98 <1105600952@qq.com>
2020-03-20 13:55:07 +0800

    ADD

---
+++ Class_5_Exe1/Class_5_Exe1/main.cpp
@@ -0,0 +1,23 @@
+#include <opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat result_1, result_2, result_3, result_4;
+	Mat srcMat = imread("G:\\jlw\\coin.png", 0);
+	Mat Core = getStructuringElement(MORPH_RECT,Size(3,3));
+	erode(srcMat, result_1, Core);
+	dilate(srcMat, result_2, Core);
+	morphologyEx(srcMat, result_3, MORPH_OPEN,Core);
+	morphologyEx(srcMat, result_4, MORPH_CLOSE, Core);
+	//threshold(srcMat, result_1, 100, 255, THRESH_BINARY);
+	//adaptiveThreshold(srcMat, result_2, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 15, 10);
+	imshow("src", srcMat);
+    imshow("res_1", result_1);
+	imshow("res_2", result_2);
+	imshow("res_3", result_3);
+	imshow("res_4", result_4);
+	waitKey(0);
+}
