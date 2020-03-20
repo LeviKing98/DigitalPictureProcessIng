99efa3963b82193a177a3df9293e7f25ce6b6adf
LeviKing98 <1105600952@qq.com>
2020-03-20 14:47:20 +0800

    ADD

--- Class_5_Exe3/Class_5_Exe3/main.cpp
+++ Class_5_Exe3/Class_5_Exe3/main.cpp
@@ -2,3 +2,51 @@
 #include <iostream>
 using namespace cv;
 using namespace std;
+
+int main()
+{
+	Mat lableMat;
+	Mat statsMat;
+	Mat centerMat;
+	Mat resultMat;
+	Mat srcMat = imread("G:\\jlw\\IMG_1989.jpg", 0);
+	Mat dstMat;
+	Mat Core = getStructuringElement(MORPH_ELLIPSE, Size(10, 10));
+	uchar j;
+
+	threshold(srcMat, dstMat, 100, 255, THRESH_BINARY);
+
+	dilate(dstMat, resultMat, Core);
+
+	threshold(srcMat, dstMat, 100, 255, THRESH_BINARY_INV);
+
+	int nComp = cv::connectedComponentsWithStats(dstMat,
+		lableMat,
+		statsMat,
+		centerMat,
+		8,
+		CV_32S);
+
+	for (int i = 1; i < nComp; i++)
+	{
+		cout << "connected Components NO." << i << endl;
+		cout << "pixels = " << statsMat.at<int>(i, 4) << endl;
+		cout << "width = " << statsMat.at<int>(i, 2) << endl;
+		cout << "height = " << statsMat.at<int>(i, 3) << endl;
+		cout << endl;
+	}
+
+	for (int i = 1; i < nComp; i++)
+	{
+		Rect bndbox;
+		bndbox.x = statsMat.at<int>(i, 0);
+		bndbox.y = statsMat.at<int>(i, 1);
+		bndbox.width = statsMat.at<int>(i, 2);
+		bndbox.height = statsMat.at<int>(i, 3);
+		rectangle(dstMat, bndbox, CV_RGB(255, 255, 255), 1, 8, 0);
+	}
+	imshow("src", srcMat);
+	imshow("dst", dstMat);
+	imshow("result", resultMat);
+	waitKey(0);
+}
