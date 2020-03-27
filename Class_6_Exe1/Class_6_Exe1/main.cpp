8dbb37329ed78c8aaae7435446edc6b9dcec8df8
LeviKing98 <1105600952@qq.com>
2020-03-27 14:04:32 +0800

    1

--- Class_6_Exe1/Class_6_Exe1/main.cpp
+++ Class_6_Exe1/Class_6_Exe1/main.cpp
@@ -3,3 +3,45 @@
 using namespace cv;
 using namespace std;
 
+int main()
+{
+	VideoCapture cap;
+	cap.open(0);
+
+	double scale = 0.5;
+
+	/*double i_minH = 0;
+	double i_maxH = 20;
+
+	double i_minS = 43;
+	double i_maxS = 255;
+
+	double i_minV = 55;
+	double i_maxV = 255;*/
+
+	while (1)
+	{
+		Mat frame;
+		//Mat hsvMat;
+		Mat detectMat;
+
+		cap >> frame;
+		Size ResImgSiz = Size(frame.cols*scale, frame.rows*scale);
+		Size CoreSiz = Size(3,3);
+		Mat rFrame = Mat(ResImgSiz, frame.type());
+		resize(frame, rFrame, ResImgSiz, INTER_LINEAR);
+
+		//cvtColor(rFrame,hsvMat, COLOR_BGR2HSV);
+
+		//rFrame.copyTo(detectMat);
+
+		//cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);
+
+		medianBlur(rFrame, detectMat, 7);
+
+		imshow("while:in the range", detectMat);
+		imshow("frame", rFrame);
+
+		waitKey(30);
+	}
+}
