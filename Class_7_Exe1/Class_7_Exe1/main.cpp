a5e11d64d6d1df4a176cc4923d939505e23bfa51
LeviKing98 <1105600952@qq.com>
2020-04-03 14:16:00 +0800

    ADD

--- Class_7_Exe1/Class_7_Exe1/main.cpp
+++ Class_7_Exe1/Class_7_Exe1/main.cpp
@@ -5,37 +5,33 @@ using namespace std;
 
 int main()
 {
-	VideoCapture cap;
-	cap.open(0);
-
-	double scale = 0.6;
 
 	while (1)
 	{
-		Mat frame;
+		Mat frame = imread("G:\\jlw\\timg.jpg",1);
+		Mat frame_gray = imread("G:\\jlw\\timg.jpg", 0);
+
 		Mat detectMatX, detectMatXAbs;
 		Mat detectMatY, detectMatYAbs;
-		Mat dstImage;
+		Mat dstImage_1, dstImage_2;
 
-		cap >> frame;
-		Size ResImgSiz = Size(frame.cols*scale, frame.rows*scale);
-		Size CoreSiz = Size(3, 3);
-		Mat rFrame = Mat(ResImgSiz, frame.type());
-		resize(frame, rFrame, ResImgSiz, INTER_LINEAR);
-		Mat GrayFrame;
-
-		//cvtColor(rFrame, GrayFrame,CV_RGB2GRAY);
-		Sobel(rFrame, detectMatX, CV_16SC1, 1, 0, 3);
+		Sobel(frame, detectMatX, CV_16SC1, 1, 0, 3);
 		convertScaleAbs(detectMatX, detectMatXAbs);
-		Sobel(rFrame, detectMatY, CV_16SC1, 0, 1, 3);
+		Sobel(frame, detectMatY, CV_16SC1, 0, 1, 3);
 		convertScaleAbs(detectMatY, detectMatYAbs);
 
-		addWeighted(detectMatXAbs, 0.5, detectMatYAbs, 0.5, 0, dstImage);
+		Canny(detectMatX,
+			detectMatY,
+			dstImage_1,
+			120, 150);
 
-		imshow("detectMatX", detectMatX);
-		imshow("detectMatY", detectMatY);
-		imshow("dstImage", dstImage);
-		imshow("frame", rFrame);
+		Canny(frame_gray, dstImage_2, 120, 150);
+		//addWeighted(detectMatXAbs, 0.5, detectMatYAbs, 0.5, 0, dstImage);
+
+		imshow("detectMatX", detectMatXAbs);
+		imshow("detectMatY", detectMatYAbs);
+		imshow("dstImage1", dstImage_1);
+		imshow("dstImage2", dstImage_2);
 
 		waitKey(30);
 	}
