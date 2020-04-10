fee46df746acd3184b00c49c95b638a5e8dd1614
LeviKing98 <1105600952@qq.com>
2020-04-10 14:47:36 +0800

    ADD

--- Class_8_Exe3/Class_8_Exe3/main.cpp
+++ Class_8_Exe3/Class_8_Exe3/main.cpp
@@ -2,3 +2,29 @@
 #include <iostream>
 using namespace cv;
 using namespace std;
+
+int main()
+{
+	Mat srcMat = imread("G://jlw//metal-part-distorted-03.png", 0);
+	Mat canny;
+	Canny(srcMat, canny, 120, 150);
+
+	vector<Vec4f> lines;
+	HoughLinesP(canny, lines, 1, CV_PI / 180,25,1,10);
+
+	Mat lineMat;
+	HoughLines(canny, lineMat, 1, CV_PI / 180,100);
+	
+	vector<Vec4f>::iterator it = lines.begin();
+	for (; it != lines.end(); ++it) {
+		Point pt1, pt2;
+		pt1.x = (*it)[0];
+		pt1.y = (*it)[1];
+		pt2.x = (*it)[2];
+		pt2.y = (*it)[3];
+		line(srcMat, pt1, pt2, Scalar(0, 0, 255), 1, CV_AA);
+	}
+	imshow("canny", canny);
+	imshow("srcMat", srcMat);
+	waitKey(0);
+}
