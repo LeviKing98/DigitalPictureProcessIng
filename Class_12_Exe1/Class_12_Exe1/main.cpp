280df74a108c9ee07782f7c873912a6651f9e9a0
LeviKing <1105600952@qq.com>
2020-05-15 14:42:03 +0800

    add

---
+++ Class_12_Exe1/Class_12_Exe1/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <opencv.hpp>
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	float gamma = 0.45;
+	float Excel[256] = { 0 };
+	for (int i = 0; i < 256; i++) {
+		Excel[i] = pow(i * 1.0 / 255, gamma);
+	}
+
+	cv::Mat srcMat = imread("‪K:\\学习\\数图\\face.jpg", 0);
+	for (int i = 0; i < srcMat.rows; i++)
+	{
+		for (int j = 0; j < srcMat.cols; j++)
+		{
+			srcMat.at<uchar>(i, j) = Excel[srcMat.at<uchar>(i, j)] * 255;
+		}
+	}
+
+	imshow("srcMat", srcMat);
+	waitKey(0);
+	return 0;
+}
