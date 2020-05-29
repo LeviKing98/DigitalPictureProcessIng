c9fb57ead1979d8994b089694d5a7d08b3ae45d4
LeviKing <1105600952@qq.com>
2020-05-29 20:14:38 +0800

    ADD

--- Class_14_Exe1/Class_14_Exe1/main.cpp
+++ Class_14_Exe1/Class_14_Exe1/main.cpp
@@ -1,145 +1,111 @@
+#include <opencv.hpp>
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#define pi 3.14
 using namespace cv;
 using namespace std;
 
-float* Cal_cellHist(Mat src);
+void SetHist(Mat srcMat, int nX, int nY, float* ref_hist);
+float CmpHist(float* ref_hist1, float* ref_hist2, int nX, int nY);
 
 int main()
 {
-	Mat img1 = imread("K:\\学习\\数图\\template.png", 0);
-	Mat srcimg2 = imread("K:\\学习\\数图\\img.png", 0);
-	int cellSize = 16;
-	int nX = img1.cols / cellSize;
-	int nY = img1.rows / cellSize;
-	Mat img2 = Mat::zeros(img1.rows, img1.cols, CV_8UC1);
-	int num = nX * nY * 8;
+	Mat orgMat = imread("K:\\学习\\数图\\img.png", 0); //241*323
+	Mat srcMat = imread("K:\\学习\\数图\\template.png", 0); //48*108
+	Mat ROIMat;
 
-	float* srcHist = new float[num];
-	memset(srcHist, 0, sizeof(float) * num);
+	Rect rect;
+	
+	int CellSize = 16;
+	int nX = srcMat.cols / CellSize;
+	int nY = srcMat.rows / CellSize;
 
-	float* img1Hist = new float[num];
-	memset(img1Hist, 0, sizeof(float) * num);
+	int bins = 8 * nX * nY;
+	float* ref_hist1 = new float[bins];
+	float* ref_hist2 = new float[bins];
 
-	float* img2Hist = new float[num];
-	memset(img2Hist, 0, sizeof(float) * num);
+	memset(ref_hist1, 0, sizeof(float) * bins);
+	memset(ref_hist2, 0, sizeof(float) * bins);
 
-	int cnt1 = 0;
-	int cnt2 = 0;
-	float distance = 0;
-	float distance_min = 10e100;
-	int x = 0;
-	int y = 0;
-	for (int q = 80; q < /*srcimg2.rows- img1.rows*/110; q++) {
-		for (int p = 80; p < /*srcimg2.cols - img1.cols*/110; p++) {
-			for (int m = 0; m < img1.rows; m++) {
-				for (int n = 0; n < img1.cols; n++) {
-					img2.at<uchar>(m, n) = srcimg2.at<uchar>(q + m, p + n);
-				}
-			}
-			//img1
-			cnt1 = 0;
-			for (int j = 0; j < nY; j++)
-			{
-				for (int i = 0; i < nX; i++)
-				{
-					Rect rectL(i * cellSize, j * cellSize, cellSize, cellSize);
-					Mat nowMat = img1(rectL);
-					for (int k = 0; k < 8; k++)
-					{
-						img1Hist[k + cnt1 * 8] = Cal_cellHist(nowMat)[k];
-					}
-					cnt1++;
-				}
-			}
-			//img2
-			cnt2 = 0;
-			for (int j = 0; j < nY; j++)
-			{
-				for (int i = 0; i < nX; i++)
-				{
-					Rect rectL(i * cellSize, j * cellSize, cellSize, cellSize);
-					Mat nowMat = img2(rectL);
-					for (int k = 0; k < 8; k++)
-					{
-						img2Hist[k + cnt2 * 8] = Cal_cellHist(nowMat)[k];
-					}
-					cnt2++;
-				}
-			}
-			distance = 0;
-			for (int i = 0; i < num; i++)
-			{
-				distance += (img1Hist[i] - img2Hist[i]) * (img1Hist[i] - img2Hist[i]);
-				if (distance < distance_min) {
-					distance_min = distance;
-					x = q;
-					y = p;
-				}
+	SetHist(srcMat, nX, nY, ref_hist2);
+
+	float dist;
+	float MinDist = 10000;
+	int tgt_x = 0;
+	int tgt_y = 0;
+	
+	for (int i=0; i < orgMat.cols- srcMat.cols; i++) {
+		for (int j=0; j < orgMat.rows-srcMat.rows; j++) {
+			rect = Rect(i, j, 108, 48);
+			ROIMat = orgMat(rect);
+			SetHist(ROIMat, nX, nY, ref_hist1);
+			dist = CmpHist(ref_hist1, ref_hist2, nX, nY);
+			if (dist < MinDist) {
+				MinDist = dist;
+				tgt_x = i;
+				tgt_y = j;
+				cout << "i = " << i << endl;
+				cout << "j = " << j << endl;
+				cout << "dist = " << dist << endl;
 			}
 		}
 	}
-	cout << "最小值" << distance_min << endl;
-	cout << "x" << x << endl;
-	cout << "y" << y << endl;
-	Rect rect(y, x, img1.cols, img1.rows);//左上坐标（x,y）和矩形的长(x)宽(y)
-	cv::rectangle(srcimg2, rect, Scalar(255, 0, 0), 1, LINE_8, 0);
-	imshow("dstMat", srcimg2);
-	waitKey(0);
-	delete[] img1Hist;
-	delete[] img2Hist;
-	return 0;
 
+	rect = Rect(tgt_x, tgt_y, 108, 48);
+	rectangle(orgMat, rect, Scalar(255, 0, 0), 1, LINE_8, 0);
+
+	imshow("原图", orgMat);
+
+	delete[] ref_hist1;
+	delete[] ref_hist2;
+
+	waitKey(0);
 }
 
-
-float* Cal_cellHist(Mat src)
+int QuantAngle(float angle)
 {
-	//计算角度及梯度
+	return (int)(angle / 45);
+}
+
+void SetHist(Mat srcMat, int nX, int nY, float* ref_hist)
+{
 	Mat gx, gy;
 	Mat mag, angle;
-	Sobel(src, gx, CV_32F, 1, 0, 1);
-	Sobel(src, gy, CV_32F, 0, 1, 1);
-	cartToPolar(gx, gy, mag, angle, true);
+	int count = -1;
+	Sobel(srcMat, gx, CV_32F, 1, 0, 1);
+	Sobel(srcMat, gy, CV_32F, 0, 1, 1);
 
-	float hist[8] = { 0 };
-	for (int j = 0; j < 16; j++)
-	{
-		for (int i = 0; i < 16; i++)
-		{
-			if (angle.at<float>(j, i) >= 0 && angle.at<float>(j, i) < 45)
-			{
-				hist[0] += mag.at<float>(j, i);
-			}
-			else if (angle.at<float>(j, i) >= 45 && angle.at<float>(j, i) < 90)
-			{
-				hist[1] += mag.at<float>(j, i);
-			}
-			else if (angle.at<float>(j, i) >= 90 && angle.at<float>(j, i) < 135)
-			{
-				hist[2] += mag.at<float>(j, i);
-			}
-			else if (angle.at<float>(j, i) >= 135 && angle.at<float>(j, i) < 180)
-			{
-				hist[3] += mag.at<float>(j, i);
-			}
-			else if (angle.at<float>(j, i) >= 180 && angle.at<float>(j, i) < 225)
-			{
-				hist[4] += mag.at<float>(j, i);
-			}
-			else if (angle.at<float>(j, i) >= 225 && angle.at<float>(j, i) < 270)
-			{
-				hist[5] += mag.at<float>(j, i);
-			}
-			else if (angle.at<float>(j, i) >= 270 && angle.at<float>(j, i) < 315)
-			{
-				hist[6] += mag.at<float>(j, i);
-			}
-			else if (angle.at<float>(j, i) >= 315 && angle.at<float>(j, i) < 360)
-			{
-				hist[7] += mag.at<float>(j, i);
+	cartToPolar(gx, gy, mag, angle, true);
+	//直方图清零
+	for (int i = 0; i < nY; i++) {
+		for (int j = 0; j < nX; j++) {
+			count++;
+			for (int m = 0; m < 16; m++) {
+				for (int n = 0; n < 16; n++) {
+					ref_hist[count * 8 + QuantAngle(angle.at<float>(i * 16 + m, j * 16 + n))] = 0;
+				}
 			}
 		}
 	}
-	return hist;
+	//直方图计算
+	count = -1;
+	for (int i = 0; i < nY; i++) {
+		for (int j = 0; j < nX; j++) {
+			count++;
+			for (int m = 0; m < 16; m++) {
+				for (int n = 0; n < 16; n++) {
+					ref_hist[count * 8 + QuantAngle(angle.at<float>(i * 16 + m, j * 16 + n))] += mag.at<float>(i * 16 + m, j * 16 + n);
+				}
+			}
+		}
+	}
 }
+
+float CmpHist(float* ref_hist1, float* ref_hist2, int nX, int nY)
+{
+	float sum = 0;
+	for (int i = 0; i < nX * nY; i++) {
+		sum += (ref_hist1[i] - ref_hist2[i]) * (ref_hist1[i] - ref_hist2[i]);
+	}
+	return sqrt(sum);
+}
+
